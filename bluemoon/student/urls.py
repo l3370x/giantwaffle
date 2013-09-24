026cd3f51412a6320eaaadd8efab5bf1679353a5
Aaron Miller <l3370x@gmail.com>
2013-09-23 22:12:39 -0600

    waffling some more

--- bluemoon/student/urls.py
+++ bluemoon/student/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('student.views',
 			url(r'^student/$', 'studentHome'),
+			url(r'^home/$', 'studentHome'),
 			url(r'^$', 'startPage'),
 			url(r'^auth/login/$', 'login'),
 			url(r'^auth/logout/$', 'logout'),
