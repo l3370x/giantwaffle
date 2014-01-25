756c456e75c0e983c4e4783b22391367abfe4a7d
Aaron Miller <l3370x@gmail.com>
2014-01-25 13:38:32 -0700

    still cleaning up

--- bluemoon/student/views.py
+++ bluemoon/student/views.py
@@ -12,8 +12,6 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 
 from student.models import *
-from news.models import *
-import pytz
 
 def startPage(request):
 	if request.user.is_authenticated():
@@ -21,40 +19,13 @@ def startPage(request):
 	return login(request)
 
 def Home(request):
-	allNews = News.objects.all().order_by('-timestamp')[:8]
 	try:
 		stud = Student.objects.get(user=request.user.id)
-		noTeam = stud.myTeam is None
-		return render(request, 'home.html', {'allNews':allNews,'username':request.user.username,'noTeam':noTeam})
+		return render(request, 'home.html', {'username':request.user.username})
 	except Student.DoesNotExist:
 		pass
-	return render(request,'home.html',{'allNews':allNews,'username':request.user.username})
+	return render(request,'home.html',{'username':request.user.username})
 
-from django.utils import timezone
-
-class TimezoneMiddleware(object):
-    def process_request(self, request):
-        tz = request.session.get('django_timezone')
-        if tz:
-            timezone.activate(tz)
-        else:
-            timezone.deactivate()
-
-def Calendar(request):
-	if request.method == 'POST':
-		request.session['django_timezone'] = pytz.timezone(request.POST['timezone'])
-	tz = request.session.get('django_timezone')
-	if tz:
-			timezone.activate(tz)
-	tzout=str(tz).replace("/","%2F")
-	try:
-		stud = Student.objects.get(user=request.user.id)
-		noTeam = stud.myTeam is None
-		return render(request,'calendar.html',{'timezones': pytz.common_timezones,'tztext':tzout,'noTeam':False},)
-	except Student.DoesNotExist:
-		pass
-	return render(request,'calendar.html',{'timezones': pytz.common_timezones,'tztext':tzout,'noTeam':True},)
-	
 
 def logout(request):
 	django.contrib.auth.logout(request)
@@ -81,57 +52,7 @@ def login(request):
 									  context_instance = RequestContext(request))
 		django.contrib.auth.login(request, user)
 		return student2Home(request, user)
-	
-def create(request):
-	d = {}
-	if request.method == 'GET':
-		form = CreateUserForm()
-		return render_to_response('student/create.html', {'form':form},
-								  context_instance = RequestContext(request))
 
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if not form.is_valid():
-			return render_to_response('student/create.html', {'form':form},
-								  context_instance = RequestContext(request))
-
-		try:
-			u = User.objects.get(username = request.POST['username'])
-			d['error'] = 'Username already taken'
-			d['form'] = form
-			return render_to_response('student/create.html', d, context_instance = RequestContext(request))
-		except User.DoesNotExist:
-			pass
-
-
-		userO = User.objects.create_user(request.POST['username'],request.POST['email'],request.POST['password']);
-		userO.save
-		person = Student.objects.create(user = userO, twitchName = request.POST['twitchName'],
-											skypeName = request.POST['skypeName'],
-											email = request.POST['email'])
-		auth_user = authenticate(username=request.POST['username'],password=request.POST['password'])
-		if auth_user is not None:
-			django.contrib.auth.login(request, auth_user)
-			News.objects.create(user=auth_user,title="New User",message="Welcome <div class='emphasis'><a href='/user/"+auth_user.username+"'>"+auth_user.username+"</a></div> to the GiantWaffle Artemis SBS Website.")
-			return Home(request)
-		d['error']="There was a server error creating your username, please try again."
-		return render_to_response('student/create.html', d, context_instance = RequestContext(request))
-
-
-
-def submitQuestion(request):
-	d = {}
-	if request.method == "GET":
-		form = HelpForm()
-		d['form']=form
-		return render(request,'help.html',d)
-	if request.method == "POST":
-		form = HelpForm(request.POST)
-		if not form.is_valid():
-			return render(request,'help.html',d)
-	send_mail(request.POST['subject'], request.POST['question'], request.POST['replyEmail'],
-    ['l3370x@gmail.com'], fail_silently=False)
-	return render(request,'helpThanks.html',d)
 
 @login_required
 def student2Home(request, studentUser):
