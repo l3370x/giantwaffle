f088c961c45bd2eccb4251e7519dc5c146256c12
Aaron Miller <l3370x@gmail.com>
2014-01-25 13:58:11 -0700

    found it..

--- bluemoon/bluemoon/settings.py
+++ bluemoon/bluemoon/settings.py
@@ -126,9 +126,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'student.views.TimezoneMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'bluemoon.urls'
